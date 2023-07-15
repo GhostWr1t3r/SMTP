1143f2341713c477124534740a9603d005dbed9c
Ali Rcham <100720880+RCHAMEXE@users.noreply.github.com>
2023-07-15 15:01:08 +0100

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -13,6 +13,10 @@ email_to = "receiver@gmail.com"   # Here you need to put the email receiver
 
 password = "" # Here you need to put your app password from your gmail ( don't forget to active 2nd verification on )
 # NOTE : if you don't know how to get the app password from your gmail go search about it on youtube or just go on my facebook profil I'will explain it www.facebook.com/3LIIX1
+# Or just watch the video : https://video.xx.fbcdn.net/v/t42.1790-2/359570867_827805811784701_6859331017310432078_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=985c63&efg=eyJ2ZW5jb2RlX3RhZyI6InN2ZV9zZCJ9&_nc_ohc=xINTQeVGb48AX9nMObk&_nc_rml=0&_nc_ht=scontent.frba2-2.fna&oh=00_AfA1JyLsftcy0HqPyyt2E2FZeGqers2vOoeMJ72VVYEXfw&oe=64B71F64
+
+
+
 
 # content of message
 
